2d2b05ef862f4e5c9552d893c11a4429aa0ae3b6
Amanpathak1 <155675919+Amanpathak1@users.noreply.github.com>
2024-04-11 10:36:58 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,9 +1,9 @@
 import os
 
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6847175705:AAHbkU8GFmzoxR9dCQTr6RuZ4NQsev5ufz0"
-auth_users = [1713924419]
-sudo_user = 1713924419
-log_channel = -1002143629860
-txt_channel = -1002049391187
+api_id = 21063898
+api_hash = "7f8faf197d0459c45c07e8f5b00faeb3"
+bot_token = "7111400415:AAGPEM099MBl1_fyXEaex82bHCC7ilyYtYE"
+auth_users = [7009461779]
+sudo_user = 7009461779
+log_channel = -
+txt_channel = -
